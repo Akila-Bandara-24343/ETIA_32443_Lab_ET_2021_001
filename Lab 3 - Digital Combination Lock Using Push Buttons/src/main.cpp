20773912626d5bd868b08d65e22c07390959bdb7
Akila-Bandara-24343 <akilalakruwan2021@gmail.com>
2026-05-20 11:47:17 +0530

    completed code with led operatios

--- Lab 3 - Digital Combination Lock Using Push Buttons/src/main.cpp
+++ Lab 3 - Digital Combination Lock Using Push Buttons/src/main.cpp
@@ -1,44 +1,54 @@
 #define F_CPU 16000000UL
-
 #include <avr/io.h>
 #include <util/delay.h>
+#include <string.h>
+
+#define PWD_LEN 4
 
 int main(void) {
-    DDRD &= ~((1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5));// PB inputs
-    DDRD |= (1 << PD6) | (1 << PD7); // LEDS
-    char pwd = '4231';
-    char inpwd;
+    DDRD &= ~((1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5)); // Inputs
+    DDRD |= (1 << PD6) | (1 << PD7);// LEDs
+
+    const char pwd[PWD_LEN + 1] = "2134";
+    char inpwd[PWD_LEN + 1];
+    uint8_t idx = 0;
+
+    memset(inpwd, 0, sizeof(inpwd));
+
     while (1) {
+        char pressed = 0;
 
-      if ((PIND & (1 << PD2)) == 0) { // Check if button 1 is pressed
-          char b1 = '2';
-          inpwd = inpwd + b1;
-          _delay_ms(100);
-        } 
-      else if ((PIND & (1 << PD3)) == 0) { // Check if button 2 is pressed
-          char b2 = '1';
-          inpwd = inpwd + b2;
-          _delay_ms(100);
-        }
-      else if ((PIND & (1 << PD4)) == 0) { // Check if button 3 is pressed
-          char b3 = '3';
-          inpwd = inpwd + b3;
-          _delay_ms(100);
-        }
-      else if ((PIND & (1 << PD5)) == 0) { // Check if button 4 is pressed
-          char b4 = '4';
-          inpwd = inpwd + b4;
-          _delay_ms(100);
+        if      ((PIND & (1 << PD2)) == 0) { pressed = '2'; }
+        else if ((PIND & (1 << PD3)) == 0) { pressed = '1'; }
+        else if ((PIND & (1 << PD4)) == 0) { pressed = '3'; }
+        else if ((PIND & (1 << PD5)) == 0) { pressed = '4'; }
+
+        if (pressed && idx < PWD_LEN) {
+            inpwd[idx++] = pressed;
+            inpwd[idx] = '\0';
+
+            // Wait for button release
+            while ((PIND & (1 << PD2)) == 0 ||
+                   (PIND & (1 << PD3)) == 0 ||
+                   (PIND & (1 << PD4)) == 0 ||
+                   (PIND & (1 << PD5)) == 0);
+
+            _delay_ms(50); // debounce delay
         }
 
-      if (inpwd == pwd) {
-          PORTD |= (1 << PD6); // Turn on Green LED
-          PORTD &= ~(1 << PD7); // Turn off RED LED
-        } else {
-          PORTD &= ~(1 << PD6); // Turn Green LED
-          PORTD |= (1 << PD7); // Turn RED LED
-        }
+        if (idx == PWD_LEN) {
+            if (strcmp(inpwd, pwd) == 0) {
+                PORTD |=  (1 << PD6); // Green ON
+                PORTD &= ~(1 << PD7); // Red OFF
+            } else {
+                PORTD &= ~(1 << PD6); // Green OFF
+                PORTD |=  (1 << PD7); // Red ON
+            }
 
+            memset(inpwd, 0, sizeof(inpwd));
+            idx = 0;
+            _delay_ms(1000); // Show result for 1s
+        }
     }
     return 0;
 }
